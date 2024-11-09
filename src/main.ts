6f4443af86dca389500d9d449d648fd94992db40
Ruslan <Giovingo92@gmail.com>
2024-11-09 14:25:29 +0200

    fix: connect

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+import { ComputerController } from './computer/computer.controller';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(ComputerController);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
